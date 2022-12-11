ec63e912829a53670ff6ee03ed5282cbdbd86768
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-12-10 23:13:38 -0300

    [*] added justify center

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,6 +4,7 @@ export const Container = styled.section`
   display: flex;
   width: 100%;
   height: 100vh;
+  justify-content: center;
 `
 export const ExercisesContainer = styled.div`
   display: flex;
