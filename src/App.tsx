7c06fe81fe502202572dce0ecab38e1be4cd34a5
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-12-21 15:23:47 -0300

    [*] added ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import GlobalStyle from 'common/styles/globalStyles'
+import Theme from 'common/styles/Theme'
 import AppProvider from 'context'
 import Routes from 'routes'
+import { ThemeProvider } from 'styled-components'
 
 function App() {
   return (
     <>
-      <AppProvider>
-        <Routes />
-        <GlobalStyle />
-      </AppProvider>
+      <ThemeProvider theme={Theme}>
+        <AppProvider>
+          <Routes />
+          <GlobalStyle />
+        </AppProvider>
+      </ThemeProvider>
     </>
   )
 }
