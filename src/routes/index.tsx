8c7c6471bb6bd597b00e216587ab0e55056b32fe
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-01-05 21:53:15 -0300

    [*] updated routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
+import { Path } from 'common/config/pathsRoutes'
+import * as Providers from 'context'
 import { Route, Routes as RoutesWrapper } from 'react-router-dom'
 import * as Pages from '../pages'
 
 export default function Routes() {
   return (
     <RoutesWrapper>
-      <Route path="/" element={<Pages.Home />} />
-      <Route path="/exercise" element={<Pages.Exercise />} />
+      <Route path={Path.Home} element={<Providers.HomeProvider />} />
+      <Route path={Path.Login} element={<Pages.Login />} />
+      <Route path={Path.Exercise} element={<Pages.Exercise />} />
     </RoutesWrapper>
   )
 }
