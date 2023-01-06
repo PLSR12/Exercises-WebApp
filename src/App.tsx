5baa7a7751ed45c2294d5e2eac1fc1558dc2c40b
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-01-06 20:57:32 -0300

    [*] added toastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import GlobalStyle from 'common/styles/globalStyles'
 import Theme from 'common/styles/Theme'
 import AppProvider from 'context/AppProvider'
+import { ToastContainer } from 'react-toastify'
 import Routes from 'routes'
 import { ThemeProvider } from 'styled-components'
 
@@ -11,6 +12,7 @@ function App() {
         <AppProvider>
           <Routes />
           <GlobalStyle />
+          <ToastContainer autoClose={3000} theme="colored" />
         </AppProvider>
       </ThemeProvider>
     </>
