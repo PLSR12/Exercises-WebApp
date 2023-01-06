f18869aa5227b7659c3767da2630521963fe53a7
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-01-05 21:52:21 -0300

    [*]  refactor page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,65 +1,14 @@
 import * as Molecules from 'components/Molecules'
-import { MenuContext } from 'context/menuContext'
-import * as React from 'react'
-import { CategoryService, ExerciseService } from 'services'
+import { HomeContext } from 'context/HomeContext'
+import React from 'react'
 import * as S from './styles'
 
-export function Home() {
-  const [modalLoadingIsOpen, setModalLoadingIsOpen] = React.useState<boolean>(true)
-  const [categories, setCategories] = React.useState<any[]>([])
-  const [exercises, setExercises] = React.useState<any[]>([])
-  const [exercisesFiltered, setExercisesFiltered] = React.useState<any[]>([])
-  const { categorySelected } = React.useContext(MenuContext)
-
-  async function getCategories() {
-    try {
-      setModalLoadingIsOpen(true)
-      const allCategories: any = await CategoryService.getAll()
-      const newCategory: any = [{ id: 0, name: 'Todos' }, ...allCategories]
-
-      setCategories(newCategory)
-      setModalLoadingIsOpen(false)
-    } catch {
-      setModalLoadingIsOpen(false)
-    }
-  }
-
-  async function getExercises() {
-    try {
-      setModalLoadingIsOpen(true)
-      const allExercises: any = await ExerciseService.getAll()
-      setExercises(allExercises)
-      setExercisesFiltered(allExercises)
-      setModalLoadingIsOpen(false)
-    } catch {
-      setModalLoadingIsOpen(false)
-    }
-  }
-
-  function getExercisesFiltered() {
-    if (categorySelected === 0) {
-      setExercisesFiltered(exercises)
-    } else if (categorySelected !== 0) {
-      const exercisesFiltered = exercises.filter(
-        (exercise: any) => categorySelected === exercise.category_id
-      )
-      setExercisesFiltered(exercisesFiltered)
-    }
-  }
-
-  React.useEffect(() => {
-    getExercisesFiltered()
-  }, [categorySelected])
-
-  React.useEffect(() => {
-    getCategories()
-    getExercises()
-  }, [])
-
+export const Home = () => {
+  const { isOpen, categories, exercisesFiltered } = React.useContext(HomeContext)
   return (
     <>
       <S.Container>
-        <Molecules.LoadingModal loading={modalLoadingIsOpen} />
+        <Molecules.LoadingModal loading={isOpen} />
         <Molecules.Header data={categories} />
         <S.ExercisesContainer>
           {exercisesFiltered.length > 0 ? (
