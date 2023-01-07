03a524ef9fdf54c4631d5b472f37a005a8410903
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-01-07 00:41:25 -0300

    [*] added route for not-found

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,6 +9,7 @@ export default function Routes() {
       <Route path={Path.Home} element={<Providers.HomeProvider />} />
       <Route path={Path.Login} element={<Pages.Login />} />
       <Route path={Path.Exercise} element={<Pages.Exercise />} />
+      <Route path="*" element={<Pages.notFound />} />
     </RoutesWrapper>
   )
 }
