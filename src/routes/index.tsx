16d7ce36bd88fe55fdb81904c64db139df24ad5c
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-01-09 22:13:37 -0300

    [*] added private routes in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,32 @@ import { Path } from 'common/config/pathsRoutes'
 import * as Providers from 'context'
 import { Route, Routes as RoutesWrapper } from 'react-router-dom'
 import * as Pages from '../pages'
+import PrivateRoute from './private-routes'
 
-export default function Routes() {
+const Routes = () => {
   return (
     <RoutesWrapper>
-      <Route path={Path.Home} element={<Providers.HomeProvider />} />
+      <Route
+        path={Path.Home}
+        element={
+          <PrivateRoute>
+            <Providers.HomeProvider />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path={Path.Exercise}
+        element={
+          <PrivateRoute>
+            <Pages.Exercise />
+          </PrivateRoute>
+        }
+      />
+
       <Route path={Path.Login} element={<Pages.Login />} />
-      <Route path={Path.Exercise} element={<Pages.Exercise />} />
       <Route path="*" element={<Pages.notFound />} />
     </RoutesWrapper>
   )
 }
+
+export default Routes
