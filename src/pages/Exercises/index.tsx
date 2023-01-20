cedd0e45adf178a96da96cd180105e776b57f2b0
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-01-20 19:49:11 -0300

    [*] updated column

--- src/pages/Exercises/index.tsx
+++ src/pages/Exercises/index.tsx
@@ -1,54 +1,56 @@
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
+import { Space } from 'antd'
+import { Path } from 'common/config/pathsRoutes'
 import * as Molecules from 'components/Molecules'
 import * as React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { ExercisesContext } from 'store/context/ExercisesContext'
 import * as S from './styles'
 
 export const Exercises = () => {
+  const navigate = useNavigate()
   const { isLoading, exercises } = React.useContext(ExercisesContext)
+  const [data, setData] = React.useState<any[]>([])
 
-  console.log(exercises)
-  console.log(isLoading)
-
-  const dataSource = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-    },
-  ]
+  React.useEffect(() => {
+    setData(exercises)
+  }, [exercises])
 
   const columns = [
+    {
+      title: 'Image',
+      dataIndex: 'url',
+      render: (t: any, r: any) => (
+        <img src={`${r.url}`} alt={`${r.name} imagem`} style={{ width: '150px', height: '15vh' }} />
+      ),
+    },
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
+      title: 'Categoria',
+      dataIndex: 'category',
+      key: 'category',
     },
     {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
+      title: 'Action',
+      dataIndex: 'operation',
+      key: 'operation',
+      render: () => (
+        <Space size="middle">
+          <EditOutlined onClick={() => navigate(Path.EditExercise)} />
+          <DeleteOutlined />
+        </Space>
+      ),
     },
   ]
 
   return (
     <S.Container>
-      <Molecules.TableComponent
-        data={dataSource}
-        columns={columns}
-        title={'Listagem de Exercícios'}
-      />
+      <Molecules.LoadingModal loading={isLoading} />
+      <Molecules.TableComponent data={data} columns={columns} title={'Listagem de Exercícios'} />
     </S.Container>
   )
 }
