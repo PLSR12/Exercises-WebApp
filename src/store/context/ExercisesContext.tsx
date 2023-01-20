34a0b6c940b11b7c4123711419ab4ca1551141b5
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-01-20 19:49:46 -0300

    [*] added formatter data

--- src/store/context/ExercisesContext.tsx
+++ src/store/context/ExercisesContext.tsx
@@ -1,13 +1,14 @@
 import { AllExercises, IExercisesListContext } from 'models/IExercises'
 import { Exercises } from 'pages/Exercises'
 import * as React from 'react'
+import { exercisesListMapper } from 'store/mapper/exercisesMapper'
 import { ExercisesService } from 'store/services'
 
 export const ExercisesContext = React.createContext<IExercisesListContext>(
   {} as IExercisesListContext
 )
 
-export const ExercisesProvider = (children: any) => {
+export const ExercisesProvider = () => {
   const [isLoading, setIsLoading] = React.useState<boolean>(true)
   const [exercises, setExercises] = React.useState<AllExercises[]>([])
 
@@ -15,7 +16,8 @@ export const ExercisesProvider = (children: any) => {
     try {
       setIsLoading(true)
       const allExercises: any = await ExercisesService.getAll()
-      setExercises(allExercises)
+      const dataFormatted = exercisesListMapper(allExercises)
+      setExercises(dataFormatted)
     } finally {
       setIsLoading(false)
     }
