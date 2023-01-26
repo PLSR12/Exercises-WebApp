11458c940221357a79601b3ceed723934a2baa30
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-01-26 17:52:55 -0300

    [*] updated styled bg color

--- src/pages/Admin/styles.ts
+++ src/pages/Admin/styles.ts
@@ -4,11 +4,12 @@ export const Container = styled.div`
   display: flex;
   min-height: 100vh;
   width: 100%;
-  background-color: #efefef;
+  background-color: ${(props) => props.theme.color.background_paper};
   justify-content: flex-start;
 `
 
 export const ContainerItems = styled.div`
   padding: 20px;
   width: 100%;
+  background-color: ${(props) => props.theme.color.background_paper};
 `
